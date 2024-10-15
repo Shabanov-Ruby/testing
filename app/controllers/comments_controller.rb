22b67185608e41173cc83cc35e889b3325a75545
Shabanov Ruby <nikshabanov57@gmail.com>
2024-10-15 22:56:56 +0300

    fix: comment undo\redo button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    @comment = @post.comments.create(comment_params.merge(user: current_user))
+    @comment = @comment.comments.create(comment_params.merge(user: current_user))
     redirect_back(fallback_location: root_path)
   end
 
@@ -24,15 +24,30 @@ class CommentsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+#  def update
+#    @comment = Comment.find(params[:id])
+#    if @comment.update(comment_params)
+#      flash[:notice] = 'Комментарий обновлен.' if @comment.saved_changes?
+#      redirect_to posts_path
+#    else
+#      redirect_to edit_post_comment_path(@comment), notice: 'Комментарий не удалось обновить.'
+#    end
+#  end
+
   def update
     @comment = Comment.find(params[:id])
-    if @comment.update(comment_params)
-      flash[:notice] = 'Комментарий обновлен.' if @comment.saved_changes?
-      redirect_to posts_path
+    if @comment.content == comment_params[:content]
+      flash[:notice] = 'Ничего не изменено. Пожалуйста, внесите изменения.'
+      render :edit
+    elsif @comment.update(comment_params)
+      flash[:notice] = 'Комментарий обновлен!' if @comment.saved_changes?
+      redirect_to root_path
     else
-      redirect_to edit_post_comment_path(@comment), notice: 'Комментарий не удалось обновить.'
+      flash[:notice] = 'Комментарий не удалось обновить.'
+      render :edit
     end
   end
+
 
   private
 
@@ -44,7 +59,7 @@ class CommentsController < ApplicationController
   end
 
   def set_post
-    @post = Post.find(params[:post_id])
+    @comment = Post.find(params[:post_id])
   end
 
   def comment_params
