0b01e4adbcea2c23d37b57a700e8a948e6dd1786
Shabanov Ruby <nikshabanov57@gmail.com>
2024-10-15 19:04:27 +0300

    fix: ui bugs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  include ActionView::Helpers::SanitizeHelper
+  before_save :format_text
   belongs_to :post
   belongs_to :user
   belongs_to :parent, class_name: 'Comment', optional: true
 
   has_many :replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy, inverse_of: :parent
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { minimum: 1, maximum: 70 }
 
   def level
     parent_id.nil? ? 0 : parent.level + 1
+  end
+  private
+
+  def format_text
+    self.content = content.gsub(/<.*?>/,'').gsub(/(.{1,20})/, "\\1\n")
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  include ActionView::Helpers::SanitizeHelper
+  before_save :format_text
   belongs_to :user
 
   has_many :comments, dependent: :destroy
 
   validates :content, presence: true, length: { minimum: 1, maximum: 90 }
+  private
+
+  def format_text
+    self.content = content.gsub(/<.*?>/,'').gsub(/(.{1,20})/, "\\1\n")
+  end
 end
