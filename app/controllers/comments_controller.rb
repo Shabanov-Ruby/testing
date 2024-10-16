99a59dd6cdf9969043b328bea8525d7060b0b8e0
Shabanov Ruby <nikshabanov57@gmail.com>
2024-10-16 14:04:06 +0300

    fix: comment delete logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,30 +5,19 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit update destroy]
 
   def create
-    @comment = @comment.comments.create(comment_params.merge(user: current_user))
+    @comment = @post.comments.create!(comment_params.merge(user: current_user))
     redirect_back(fallback_location: root_path)
   end
 
   def edit; end
-  # TODO: написать джобу и вставить этот код
-
-  # ActiveRecord::Base.transaction do
-  #   delete_sub_comments(@comment)
-  #   @comment.destroy!
-  # end
-
-  # def delete_sub_comments(comment)
-  #  comment.replies.each do |child|
-  #    delete_sub_comments(child)
-  #    child.destroy!
-  #  end
-  # end
 
   def destroy
-    # @comment.soft_delete
+    #if @comment.user == current_user
+    #  @comment.soft_delete
     #  flash[:notice] = 'Комментарий успешно удален.'
-    # redirect_back(fallback_location: root_path)
-
+    #end
+    #redirect_back(fallback_location: root_path)
+    
     @comment = Comment.find(params[:id])
 
     if @comment.user == current_user
@@ -41,33 +30,32 @@ class CommentsController < ApplicationController
   end
 
   def update
-    @comment = Comment.find(params[:id])
-    if @comment.content == comment_params[:content]
-      flash[:notice] = 'Ничего не изменено. Пожалуйста, внесите изменения.'
-      render :edit
-    elsif @comment.update(comment_params)
-      flash[:notice] = 'Комментарий обновлен!' if @comment.saved_changes?
-      redirect_to root_path
+    if @comment.update(comment_params)
+      flash[:notice] = 'Комментарий обновлен.' 
+      redirect_to posts_path
     else
-      flash[:notice] = 'Комментарий не удалось обновить.'
-      render :edit
+      redirect_to edit_post_comment_path(@comment), notice: 'Комментарий не удалось обновить.'
     end
   end
 
   private
 
-  def delete_sub_comments(comment)
-    comment.replies.each do |child|
-      delete_sub_comments(child)
-      child.destroy
-    end
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
-  def set_comment
-    @comment = current_user.comments.find(params[:id])
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 
   def comment_params
     params.require(:comment).permit(:content, :parent_id)
+  end
+
+  def delete_sub_comments(comment)
+    comment.replies.each do |child|
+      delete_sub_comments(child)
+      child.destroy!
+    end
   end
 end
